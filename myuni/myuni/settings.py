51307b211cb007bdd02ddbefa0a17b3501a7cc19
n105na <ambenaini@gmail.com>
2024-12-12 23:21:31 +0100

    dep

--- myuni/myuni/settings.py
+++ myuni/myuni/settings.py
@@ -34,8 +34,8 @@ ALLOWED_HOSTS = ["*"]
 <<<<<<< HEAD
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 =======
-ALLOWED_HOSTS = ['*']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 >>>>>>> d045b7998789e7e768da681aaa3c2a8e80b9836c
 
